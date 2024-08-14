b5b8a750b8a15a78293a528f3aa0ee32fd03a3d2
Matt Henroid <matt.henroid@daugherty.com>
2024-08-14 13:09:31 -0500

    Adding simple example for why we wrap MUI in a ThemeProvider

--- demo-app/src/app/layout.tsx
+++ demo-app/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "../theme/ThemeProvider";
 import { Container } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@/components/navBar/NavBar";
+// import { ThemeProvider as MuiThemeProvider } from "@mui/material";
+// import theme from "@/theme/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +32,16 @@ export default function RootLayout({
             </main>
           </div>
         </ThemeProvider>
+        {/* EXAMPLE: THIS WILL NOT WORK BECAUSE ALL CLIENT SPECIFIC CODE MUST BE WRAPPED IN A CLIENT COMPONENT */}
+        {/* <MuiThemeProvider theme={theme}>
+          <div>
+            <CssBaseline />
+            <AppBar />
+            <main>
+              <Container maxWidth={false}>{children}</Container>
+            </main>
+          </div>
+        </MuiThemeProvider> */}
       </body>
     </html>
   );
